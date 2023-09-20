762ad258c1fec8289cf800993fda816ba5ae8390
Good Mood <110936205+g00dm00d@users.noreply.github.com>
2023-09-20 15:08:37 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      roboto: ["Roboto", "ui-monospace", "SFMono-Regular"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ["Open Sans"],
+    },
+  },
+  plugins: [],
+};
